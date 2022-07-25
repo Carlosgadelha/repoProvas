809f8526052a6bf60b4d12323ec800849ca874ee
Ceso tarde <cesotardesedhas@gmail.com>
2022-07-25 16:26:17 -0300

    concluindo tests

--- src/routers/testsRouter.ts
+++ src/routers/testsRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { createTest, testPerCategories, testPerTeachers } from "../controllers/testsController.js";
+import { validateToken } from "../middlewares/validateToken.js";
 
 const testRouter = Router();
 
 testRouter.post("/test", createTest )
-testRouter.get("/testPerCategories", testPerCategories)
-testRouter.get("/testPerTeachers", testPerTeachers)
+testRouter.get("/testPerCategories", validateToken, testPerCategories)
+testRouter.get("/testPerTeachers",validateToken, testPerTeachers)
 
 export default testRouter;
